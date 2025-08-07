cedbe77b22d40d8566c053d082d2c5521c99a5a5
Resource-02 <resource2.code4bharat@gmail.com>
2025-08-07 16:06:19 +0530

    update page.jsx, page.jsx and sidebar.jsx

--- src/app/tickets/page.jsx
+++ src/app/tickets/page.jsx
@@ -1,11 +1,14 @@
 //src/app/adminticket/page.jsx
 import React from 'react';
 import TicketsSection from '@/components/ticket/ticket'; // If using alias
+import ProtectedRoute from '@/components/ProtectedRoute';
 
 export default function page() {
   return (
+     <ProtectedRoute>
     <div>
       <TicketsSection />
     </div>
+    </ProtectedRoute>
   );
 }
