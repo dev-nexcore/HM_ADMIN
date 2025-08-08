92398537753240055ba30b7e164283acc669ea69
Resource-02 <resource2.code4bharat@gmail.com>
2025-08-08 11:28:24 +0530

    update login.jsx

--- src/components/adminlogin/login.jsx
+++ src/components/adminlogin/login.jsx
@@ -23,7 +23,7 @@ const AdminLogin = () => {
     setErrorMsg("");
     
     try {
-      const response = await fetch('/api/auth/login', {
+      const response = await fetch('/api/adminauth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
