090ebf257abcec8de180536dcf8d4c24cea4768e
Akshi1277 <verstapenalfred@gmail.com>
2025-11-12 15:20:59 +0530

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/admin',
   assetPrefix: '/admin',
-  trailingSlash: true,
-  // This is important - tells Next.js to output standalone build
-  output: 'standalone'
+  trailingSlash: true
 };
 
 export default nextConfig;
